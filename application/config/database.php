54375900403d1b41a43f9ba191e6c6b7aa8400e3
Matjaž Horvat <matjaz.horvat@gmail.com>
2014-01-17 08:53:18 +0100

    We don't need to stor DB password in git

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'password';
+$db['default']['password'] = '';
 $db['default']['database'] = 'fusioninvoice';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
