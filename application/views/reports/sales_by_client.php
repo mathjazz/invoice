11bc7581fc1ff697f40e3222632bda22f740f8cf
Matjaž Horvat <matjaz.horvat@gmail.com>
2014-12-27 11:33:44 +0100

    Remove sales without tax column from sales by client report

--- application/views/reports/sales_by_client.php
+++ application/views/reports/sales_by_client.php
@@ -11,14 +11,12 @@
 			<tr>
 				<th><?php echo lang('client'); ?></th>
 				<th class="amount"><?php echo lang('invoice_count'); ?></th>
-				<th class="amount"><?php echo lang('sales'); ?></th>
 				<th class="amount"><?php echo lang('sales_with_tax'); ?></th>
 			</tr>
 			<?php foreach ($results as $result) { ?>
 			<tr>
 				<td><?php echo $result->client_name; ?></td>
 				<td class="amount"><?php echo $result->invoice_count; ?></td>
-				<td class="amount"><?php echo format_currency($result->sales); ?></td>
 				<td class="amount"><?php echo format_currency($result->sales_with_tax); ?></td>
 			</tr>
 			<?php } ?>
